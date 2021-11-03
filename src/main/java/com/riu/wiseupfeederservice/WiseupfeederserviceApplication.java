8e29db1cc5025707909713f31c2e7956f2339976
Juan David <saavedravillalobosjuan@gmail.com>
2021-11-03 12:20:08 -0300

    commit init

---
+++ src/main/java/com/riu/wiseupfeederservice/WiseupfeederserviceApplication.java
@@ -0,0 +1,13 @@
+package com.riu.wiseupfeederservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WiseupfeederserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WiseupfeederserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/riu/wiseupfeederservice/WiseupfeederserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.riu.wiseupfeederservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WiseupfeederserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
